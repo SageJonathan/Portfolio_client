f13d8b65bd8dbb7c37672733d99bf314230edef7
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-07-09 17:11:10 -0700

    deploy logo

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,11 @@
 import "./Nav.scss";
+import sagecodes from "../../assets/logos/sagecodes.png"
 
 function Nav() {
   return (
     <div className="nav">
       <div className="nav__img-container">
-        <img className="nav__img" src="" alt="LOGO" />
+        <img className="nav__img" src={sagecodes} alt="LOGO" />
       </div>
       <ul className="nav__link-container">
         <li className="nav__link">About</li>
