577c58fa289106bcfd0d92ef48f1128825f585dd
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-07-10 15:58:38 -0700

    add dummy thumbnail. base html

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,34 @@
 import "./Projects.scss"
+import thumbnail from "../../assets/images/thumbnail.png"
 
 function Projects() {
 
     return (
      <div>
-     
+      <div className="project__container">
+          <div className="video__container">
+            <img src={thumbnail} alt="" />
+          </div>
+          <div className="skills__container">
+
+          </div>
+      </div>
+      <div className="project__container">
+         <div className="video__container">
+         <img src={thumbnail} alt="" />
+         </div>
+         <div className="skills__container">
+
+         </div>
+      </div>
+      <div className="project__container">
+        <div className="video__container">
+        <img src={thumbnail} alt="" />
+        </div>
+        <div className="skills__container">
+
+        </div>
+      </div>
      </div>
     )
   }
