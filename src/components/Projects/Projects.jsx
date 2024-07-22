842179c12f96df25e18bb016f5c54b047289741a
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-07-22 13:03:51 -0600

    layout proj skeleton

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -11,24 +11,27 @@ function Projects() {
           <div className="video__container">
             <img src={thumbnail} alt="" />
           </div>
-          <div className="skills__container">
-            <h3 className="skills__header">Avalanche AI</h3>
+          <div className="description__container">
+            <h3 className="description__header">Avalanche AI</h3>
+            <p className="description__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci earum saepe similique ex aut quaerat doloremque natus cum nulla. Voluptas pariatur doloremque non sunt nesciunt totam similique quod! Quia, placeat!</p>
           </div>
       </div>
       <div className="project__container project__container--2">
          <div className="video__container">
          <img src={thumbnail} alt="" />
          </div>
-         <div className="skills__container">
-            <h3 className="skills__header">Warehouse Tracker</h3>
+         <div className="description__container">
+            <h3 className="description__header">Warehouse Tracker</h3>
+            <p className="description__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima culpa, architecto perspiciatis nisi asperiores incidunt reprehenderit ipsa veniam! Quis blanditiis magnam minima qui, quidem doloremque recusandae reprehenderit hic iusto. Est.</p>
          </div>
       </div>
       <div className="project__container project__container--3">
         <div className="video__container">
         <img src={thumbnail} alt="" />
         </div>
-        <div className="skills__container">
-            <h3 className="skills__header">Microsoft Hackaton</h3>
+        <div className="description__container">
+            <h3 className="description__header">Microsoft Hackaton</h3>
+            <p className="description__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat explicabo eveniet quaerat exercitationem pariatur, in sequi necessitatibus aliquid, eligendi magni cupiditate repudiandae? Illum suscipit dicta corporis! Minus esse corrupti assumenda!</p>
         </div>
       </div>
      </div>
