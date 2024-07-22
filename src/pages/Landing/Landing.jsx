bbd648dad3ddd381413e18efb1bc15ecb5b61dff
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-07-22 13:58:18 -0600

    add skills icons and establiosh layout

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,6 +2,7 @@ import "./Landing.scss"
 import About from "../../components/About/About"
 import Projects from "../../components/Projects/Projects"
 import Contact from "../../components/Contact/Contact"
+import Skills from "../../components/Skills/Skills"
 
 function Landing () {
 
@@ -10,6 +11,7 @@ function Landing () {
    
      <About></About>
      <Projects></Projects>
+     <Skills></Skills>
      <Contact></Contact>
      </>
     )
