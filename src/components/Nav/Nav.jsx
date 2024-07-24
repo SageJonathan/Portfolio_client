a5d2534643ae129c7f16520f75cb14eddd6b872f
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-07-24 10:42:28 -0600

    add scroll links in nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,20 +1,55 @@
+// import "./Nav.scss";
+// import sagecodes from "../../assets/logos/hummingbird.ico"
+
+
+
+// function Nav() {
+//   return (
+  
+//     <div className="nav">
+//       <div className="nav__img-container">
+//         <img className="nav__img" src={sagecodes} alt="LOGO" />
+//       </div>
+//       <ul className="nav__link-container">
+//         <li className="nav__link">About <span className="nav__link-dash">|</span></li>
+//         <li className="nav__link">Projects <span className="nav__link-dash">|</span></li>
+//         <li className="nav__link nav__link--contact">Contact <span className="nav__link-dash">|</span></li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Nav;
+import React from 'react';
+import { Link } from 'react-scroll';
+import { useNavigate } from 'react-router-dom';
 import "./Nav.scss";
-import sagecodes from "../../assets/logos/hummingbird.ico"
+import sagecodes from "../../assets/logos/hummingbird.ico";
 
 function Nav() {
+    const navigate = useNavigate();
   return (
-  
     <div className="nav">
-      <div className="nav__img-container">
-        <img className="nav__img" src={sagecodes} alt="LOGO" />
+      <div className="nav__img-container" onClick={() => navigate('/')}>
+        <img className="nav__img" src={sagecodes} alt="LOGO"/>
       </div>
       <ul className="nav__link-container">
-        <li className="nav__link">About <span className="nav__link-dash">|</span></li>
-        <li className="nav__link">Projects <span className="nav__link-dash">|</span></li>
-        <li className="nav__link nav__link--contact">Contact <span className="nav__link-dash">|</span></li>
+        <li className="nav__link">
+          <Link to="about" smooth={true} duration={500}>About</Link>
+          <span className="nav__link-dash">|</span>
+        </li>
+        <li className="nav__link">
+          <Link to="projects" smooth={true} duration={500}>Projects</Link>
+          <span className="nav__link-dash">|</span>
+        </li>
+        <li className="nav__link nav__link--contact">
+          <Link to="contact" smooth={true} duration={500}>Contact</Link>
+          <span className="nav__link-dash">|</span>
+        </li>
       </ul>
     </div>
   );
 }
 
 export default Nav;
+
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,24 +1,38 @@
-import "./Landing.scss"
-import Hero from "../../components/Hero/Hero"
-import Projects from "../../components/Projects/Projects"
-import Contact from "../../components/Contact/Contact"
-import Skills from "../../components/Skills/Skills"
-import About from "../../components/About/About"
-import Mission from "../../components/Mission/Mission"
+import React from 'react';
+import { Element } from 'react-scroll';
+import "./Landing.scss";
+import Hero from "../../components/Hero/Hero";
+import Projects from "../../components/Projects/Projects";
+import Contact from "../../components/Contact/Contact";
+import Skills from "../../components/Skills/Skills";
+import About from "../../components/About/About";
+import Mission from "../../components/Mission/Mission";
 
-function Landing () {
+function Landing() {
+  return (
+    <>
+      <Element name="hero">
+        <Hero />
+      </Element>
+      <Element name="about">
+        <About />
+      </Element>
+      <Element name="projects">
+        <Projects />
+      </Element>
+      <Element name="skills">
+        <Skills />
+      </Element>
+      <Element name="mission">
+        <Mission />
+      </Element>
+      <Element name="contact">
+        <Contact />
+      </Element>
+    </>
+  );
+}
 
-    return (
-     <>
-     <Hero></Hero>
-     <About></About>
-     <Projects></Projects>
-     <Skills></Skills>
-     <Mission></Mission>
-     <Contact></Contact>
-     </>
-    )
-  }
-  
-  export default Landing
+export default Landing;
+
   
