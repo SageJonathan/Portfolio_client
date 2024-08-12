02138ab01f6a7ddaf4143ad5f0ad58451e8034b4
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-08-12 11:53:04 -0400

    add view only aniamtion on mission

--- src/components/Mission/Mission.jsx
+++ src/components/Mission/Mission.jsx
@@ -1,11 +1,48 @@
-import "./Mission.scss";
+import React, { useEffect, useRef, useState } from 'react';
+import './Mission.scss';
+
+
+function useOnScreen(ref) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target); 
+        }
+      },
+      { threshold: 0.1} 
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current); 
+      }
+    };
+  }, [ref]);
+
+  return isVisible;
+}
 
 function Mission() {
+  const headerRef = useRef(null);
+  const isHeaderVisible = useOnScreen(headerRef);
+
   return (
     <div className="mission">
-      <h2 className="mission__header">Mission Statement</h2>
+      <h2
+        ref={headerRef}
+        className={`mission__header ${isHeaderVisible ? 'animate' : ''}`}
+      >
+        Mission Statement
+      </h2>
       <p className="mission__text">
-        {" "}
         I am committed to redefining what is possible.
       </p>
       <p className="mission__text">
