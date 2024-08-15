e06ad2c974a26580aa26b2679d023c1065d18834
Jonathan Sage <sagejonathan.tesol@gmail.com>
2024-08-15 12:21:14 -0400

    adjust initial change

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,22 +3,21 @@ import Author3 from "../../assets/images/author3.jpg";
 
 function Hero() {
   return (
-    <>
       <div className="hero">
         <div className="person">
           <div className="person__container">
             <div className="person__text--upper">
               <span className="person__spread--gold"></span>
               <h1 className="person__name">
-                Jonathan Sage<span className="person__dot"></span>
+                {/* Jonathan Sage<span className="person__dot"></span> */}
               </h1>
             </div>
             <div className="person__text--lower">
               <div className="person__spread--cyan"></div>
-              <p className="person__role">Fullstack Developer</p>
+              {/* <p className="person__role">Fullstack Developer</p> */}
             </div>
             <div className="person__img-container">
-              <img className="person__img" src={Author3} alt="author" />
+              {/* <img className="person__img" src={Author3} alt="author" /> */}
             </div>
           </div>
         </div>
@@ -27,7 +26,6 @@ function Hero() {
           <h3 className="text__subheader">Robust. Clean. Scalable.</h3>
         </div>
       </div>
-    </>
   );
 }
 
