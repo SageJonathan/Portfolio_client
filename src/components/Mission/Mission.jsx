6bb24c2808474bcedc3c5991d73fb872658812bc
Jonathan Sage <sagejonathan.tesol@gmail.com>
2025-04-12 14:37:17 -0600

    we to me

--- src/components/Mission/Mission.jsx
+++ src/components/Mission/Mission.jsx
@@ -13,10 +13,10 @@ function Mission() {
         Mission Statement
       </h2>
       <p className="mission__text">
-        We are committed to redefining what is possible.
+        I am committed to redefining what is possible.
       </p>
       <p className="mission__text">
-      Our mission is to deliver innovative, user-centric solutions
+      My mission is to deliver innovative, user-centric solutions
       <br></br>That solve real-world problems and empower businesses to scale.
       </p>
       
