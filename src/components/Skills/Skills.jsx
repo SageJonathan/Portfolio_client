388234a6e0a151973aeb9ab10db8ce5662570049
Jonathan Sage <sagejonathan.tesol@gmail.com>
2025-04-12 10:50:38 -0600

    rempove some skills and migrate skill icons to subbset asset folder

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,15 +1,13 @@
 import "./Skills.scss";
-import html from "../../assets/images/html.png";
-import css from "../../assets/images/css.png";
-import sass from "../../assets/images/sass.png";
-import js from "../../assets/images/javascript.png";
-import node from "../../assets/images/node.png";
-import react from "../../assets/images/react.svg";
-import express from "../../assets/images/express.png";
-import axios from "../../assets/images/axios.png";
-import mysql from "../../assets/images/mysql.png";
-import knex from "../../assets/images/knex.png";
-import figma from "../../assets/images/figma.png";
+import html from "../../assets/icons/html.png";
+import css from "../../assets/icons/css.png";
+import sass from "../../assets/icons/sass.png";
+import js from "../../assets/icons/javascript.png";
+import node from "../../assets/icons/node.png";
+import react from "../../assets/icons/react.svg";
+import express from "../../assets/icons/express.png";
+import mysql from "../../assets/icons/mysql.png";
+import figma from "../../assets/icons/figma.png";
 
 function Skills() {
   return (
@@ -22,10 +20,8 @@ function Skills() {
           <img className="skills__icons" src={sass} alt="" />
           <img className="skills__icons" src={js} alt="" />
           <img className="skills__icons" src={react} alt="" />
-          <img className="skills__icons" src={axios} alt="" />
           <img className="skills__icons" src={node} alt="" />
           <img className="skills__icons" src={express} alt="" />
-          <img className="skills__icons" src={knex} alt="" />
           <img className="skills__icons" src={mysql} alt="" />
         </div>
         <div className="skills__images-contianer__mobile">
@@ -37,12 +33,10 @@ function Skills() {
         <div className="skills__images-contianer__mobile">
           <img className="skills__icons" src={js} alt="" />
           <img className="skills__icons" src={react} alt="" />
-          <img className="skills__icons" src={axios} alt="" />
         </div>
         <div className="skills__images-contianer__mobile">
           <img className="skills__icons" src={node} alt="" />
           <img className="skills__icons" src={express} alt="" />
-          <img className="skills__icons" src={knex} alt="" />
           <img className="skills__icons" src={mysql} alt="" />
         </div>
       </div>
